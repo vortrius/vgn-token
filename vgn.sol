782f7c44e8d17dc6962cde92dd7c3fa4530d6678
Aaron Chavarría <aronchj7@live.com>
2024-01-16 11:22:51 -0600

    Update vgn.sol

--- vgn.sol
+++ vgn.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/// @custom:security-contact security@valiants.io
+/// @custom:security-contact support@vortrius.com
 contract VGN is ERC20 {
   constructor() ERC20("Vortrius Game Network", "VGN") {
     _mint(msg.sender, 20_000_000e18);
